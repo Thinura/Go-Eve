ecb99e9b0cf918b64f9a0a876c19f36e03ce82c2
Thinura Laksara <thinuralaksara@gmail.com>
2019-02-24 02:40:03 +0530

    Splash screen is done

---
+++ frontend/goeve/android/app/src/main/java/com/goeve/MainActivity.java
@@ -0,0 +1,24 @@
+package com.goeve;
+
+import android.os.Bundle;
+
+import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
+
+public class MainActivity extends ReactActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SplashScreen.show(this);
+    }
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "goeve";
+    }
+}
